2add76bc9c7be51fed93f85810211bbcd5ef32f3
Chris Barrick <cbarrick1@gmail.com>
2014-07-13 23:44:20 -0400

    Complete rewrite!!
    - The code is simpler and easier to read
    - You can now give standard input to `run(...).exec([stdin], [callback])`
        as a String, Buffer, or Vinyl file
    - A new `Command` class is exposed for running commands outside of gulp
    - Fixes #8
    - No need to set the max listeners on `process.stdout` (#7)
    - Removed dependency on CircularBuffer

--- README.md
+++ README.md
@@ -8,66 +8,116 @@ Many Unix commands are built around the idea of piping. Let's take advantage of 
 Usage
 --------------------------------------------------
 
-### `var run = require('gulp-run')`
+```javascript
+var run = require('gulp-run');
+
+
+// Use gulp-run to start a pipeline
+gulp.task('hello-world', function () {
+  run('echo Hello World').exec()  // prints "Hello World\n"
+    .pipe(gulp.dest('output'))    // Writes "Hello World\n" to output/echo
+})
+
+
+// Use gulp-run in the middle of a pipeline:
+gulp.task('even-lines', function () {
+  gulp.src('path/to/input/*')           // Get input files.
+    .pipe(run('awk "NR % 2 == 0"'))     // Use awk to extract the even lines.
+    .pipe(gulp.dest('path/to/output'))  // Profit.
+});
+
+
+// Use gulp-run without gulp
+var cmd = new run.Command('cat');  // Create a command object for `cat`
+cmd.exec('hello world');           // Call `cat` with 'hello world' on stdin
+```
 
 
 API
 --------------------------------------------------
 
-### `var cmd = run(command, [options])`
+### `run(template, [options])`
 
-Gets a through stream for a shell command to which you can pipe vinyl files. For each file piped, a new process is spawned, the file is read into the processes's stdin, and a file containing the processes's stdout is pushed.
+Creates a Vinyl (gulp) stream that transforms its input by piping it to a shell command.
 
-Additionally, `./node_modules/.bin` is prepended to the PATH for the child process, so you have access to all the binaries provided by your module's dependencies.
-
-### Arguments
-1. `command` *(String)*: The command to run. It can be a [template] interpolating the vinyl file
-    as the variable `file`.
-2. `[options]` *(Object)*:
-    - `env` *(Object)*: The environmental variables for the child process. Defaults to
-        `process.env`. The path `node_modules/.bin` is automatically prepended to the PATH.
-    - `cwd` *(String)*: The initial working directory for the child process. Defaults to
-        `process.cwd()`.
-    - `silent` *(Boolean)*: If true, do not print the command's output. This is the same as
-        setting verbosity to 1. Defaults to `false`.
-    - `verbosity` *(Number)*: Sets the verbosity level. Defaults to `2`.
-        - `0` never outputs anything.
-        - `1` outputs basic logs.
-        - `2` outputs basic logs and the stdout of the child process.
+See <a href="#run.Command">`run.Command`</a> for a description of the arguments.
 
 #### Returns
-*(Stream.Transform in Object Mode)*: The through stream you so desire.
+
+*(stream.Transform in Object Mode)*: Returns a Transform stream that receives Vinyl files. For each input, a sub process is started taking the contents of the Vinyl file as standard input and a new file is pushed downstream containing the process's standard output.
 
 #### Example
 ```javascript
 gulp.task('even-lines', function () {
-    gulp.src('path/to/input/*')            // Get input files.
-        .pipe(run('awk "NR % 2 == 0"'))    // Use awk to extract the even lines.
-        .pipe(gulp.dest('path/to/output')) // Profit.
+  gulp.src('path/to/input/*')           // Get input files.
+    .pipe(run('awk "NR % 2 == 0"'))     // Use awk to extract the even lines.
+    .pipe(gulp.dest('path/to/output'))  // Profit.
 })
 ```
 
+---
 
-### `cmd.exec([callback])`
+### `run(...).exec([stdin], [callback])`
 
-Executes the command immediately, returning the output as a vinyl stream. Unless the `silent` option is true, the output is tee'd to `process.stdout` with each line prepended by the string **"[*title*] "** where *title* is the command's name.
-
-The name of the file pushed down the pipe is the first word of the command. See [gulp-rename] if you need more flexibility.
+Start a gulp pipeline and execute the command immediately pushing the results downstream.
 
 #### Arguments
-1. `[callback]` *(Function)*: Execution is asynchronous. The callback is called once the
-    command's stdout has closed.
+
+1. `[stdin]` *(String | Buffer | Vinyl)*: If given, this will be used as stdin for the command.
+
+2. `[callback]` *(Function)*: The callback is called once the command has exited. An Error is passed if the exit status was non-zero. The error will have a `status` property set to the exit status.
 
 #### Returns
-*(Stream.Readable in Object Mode)*: A stream containing exactly one vinyl file. The file's contents is the stdout stream of the command.
+*(Stream.Readable in Object Mode)*: Returns a Vinyl (gulp) stream which will push downstream the stdout of the command as a Vinyl file. The default path of the Vinyl file is the first word of the template; use [gulp-rename] for more versatility.
 
 #### Example
 ```javascript
 gulp.task('hello-world', function () {
-    run('echo Hello World').exec() // prints "[echo] Hello World\n"
-        .pipe(gulp.dest('output')) // Writes "Hello World\n" to output/echo
+  run('echo Hello World').exec()  // prints "[echo] Hello World\n"
+    .pipe(gulp.dest('output'))    // Writes "Hello World\n" to output/echo
 })
 ```
+
+---
+
+<a name="run.Command"></a>
+### `new run.Command(template, [options])`
+Represents a command to be run in a subshell.
+
+#### Arguments
+
+1. `template` *(String)*: The command to run. It can be a [template] interpolating the vinyl file as the variable `file`. The template need not interpolate anything. A simple shell command will do just fine. The command is passed as an argument to `sh -c`, so I/O redirection and the like will work as you would expect from a terminal.
+
+2. `options` *(Object)*:
+    - `env` *(Object)*: The environmental variables for the child process. Defaults to `process.env`. Your `node_modules/.bin` is automatically prepended to the PATH.
+    - `cwd` *(String)*: The initial working directory for the child process. Defaults to `process.cwd()`.
+    - `silent` *(Boolean)*: If true, do not print the command's output. This is the same as setting verbosity to 1. Defaults to false.
+    - `verbosity` *(Number)*: Sets the verbosity level. Defaults to `2`.
+        - `0`: Do not print anything, ever.
+        - `1`: Print the command being run and its stderr.
+        - `2`: Print the command, its stderr, and its stdout.
+
+---
+
+### `run.Command#exec([stdin], [callback])`
+Spawn a subshell and execute the command.
+
+#### Arguments
+
+1. `[stdin]` *(String | Buffer | Vinyl)*: If given, this will be used as stdin for the command.
+
+2. `[callback]` *(Function)*: The callback is called once the command has exited. An Error is passed if the exit status was non-zero. The error will have a `status` property set to the exit status.
+
+#### Returns
+*(Vinyl)*: Returns a [Vinyl] file wrapping the stdout of the command.
+
+#### Example
+```javascript
+var cmd = new run.Command('cat');  // Create a command object for `cat`
+cmd.exec('hello world');           // Call `cat` with 'hello world' on stdin
+```
+
+---
 
 
 The ISC License
@@ -85,3 +135,4 @@ THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH RE
 [gulp-shell]: https://github.com/sun-zheng-an/gulp-shell
 [gulp-spawn]: https://github.com/hparra/gulp-spawn
 [template]: http://lodash.com/docs#template
+[Vinyl]: https://github.com/wearefractal/vinyl
--- gulp-run.js
+++ gulp-run.js
@@ -4,216 +4,34 @@
 
 'use strict';
 
-var childProcess = require('child_process');
-var pathlib = require('path');
-var stream = require('stream');
+var Transform = require('stream').Transform;
 
-var _ = require('lodash');
-var color = require('cli-color');
-var Vinyl = require('vinyl');
-
-var Logger = require('./lib/logger');
+var Command = require('./lib/command');
 
 
-/// `var cmd = run(command, [options])`
-/// --------------------------------------------------
-/// Gets a through stream for a shell command to which you can pipe vinyl files. For each file
-/// piped, a new process is spawned, the file is read into the processes's stdin, and a file
-/// containing the processes's stdout is pushed.
-///
-/// Additionally, `./node_modules/.bin` is prepended to the PATH for the child process, so you have
-/// access to all the binaries provided by your module's dependencies.
-///
-/// ### Arguments
-/// 1. `command` *(String)*: The command to run. It can be a [template] interpolating the vinyl file
-///     as the variable `file`.
-/// 2. `[options]` *(Object)*:
-///     - `env` *(Object)*: The environmental variables for the child process. Defaults to
-///         `process.env`. The path `node_modules/.bin` is automatically prepended to the PATH.
-///     - `cwd` *(String)*: The initial working directory for the child process. Defaults to
-///         `process.cwd()`.
-///     - `silent` *(Boolean)*: If true, do not print the command's output. This is the same as
-///         setting verbosity to 1. Defaults to `false`.
-///     - `verbosity` *(Number)*: Sets the verbosity level. Defaults to `2`.
-///         - `0` never outputs anything.
-///         - `1` outputs basic logs.
-///         - `2` outputs basic logs and the stdout of the child process.
-///
-/// [template]: http://lodash.com/docs#template
-///
-/// ### Returns
-/// *(stream.Transform in Object Mode)*: The through stream you so desire.
-///
-/// ### Example
-/// ```javascript
-/// gulp.task('even-lines', function () {
-///     gulp.src('path/to/input/*')            // Get input files.
-///         .pipe(run('awk "NR % 2 == 0"'))    // Use awk to extract the even lines.
-///         .pipe(gulp.dest('path/to/output')) // Profit.
-/// })
-/// ```
-
-module.exports = function run(command, opts) {
-	var commandStream = new stream.Transform({objectMode: true}); // The stream of vinyl files.
-
-	// Options
-	opts = _.defaults(opts||{}, {
-		cwd: process.cwd(),
-		env: process.env,
-		silent: false,
-		verbosity: (opts && opts.silent) ? 1 : 2
-	});
-
-	// If `opts.silent` and `opts.verbosity` disagree, verbosity wins
-	opts.silent = (opts && opts.verbosity < 2) ? true : false;
-
-	// Compile the command template.
-	var commandTemplate = _.template(command);
-
-
-	// exec(command, [input], [callback])
-	// --------------------------------------------------
-	// TODO: Document
-
-	function exec(command, input, callback) {
-		var child; // The child process.
-		var outStream; // The contents of the returned vinyl file.
-
-		// Parse arguments.
-		if (typeof arguments[1] === 'function') {
-			input = null;
-			callback = arguments[1];
-		}
-
-		// Setup logging.
-		var logger = new Logger(opts.verbosity);
-		logger.stream.pipe(process.stdout);
-
-		// Log start message.
-		var startMessage = '$ ' + color.cyan(command);
-		if (input && input.relative) {
-			startMessage += ' < ' + color.magenta(input.relative);
-		}
-		if (opts.silent) {
-			startMessage += color.blackBright(' # Silenced');
-		}
-		logger.log(1, startMessage);
-
-		// Setup environment of child process.
-		opts.env.PATH = pathlib.join(__dirname, '../../node_modules', '.bin') + ':' + opts.env.PATH;
-
-		// Spawn the process.
-		child = childProcess.spawn('sh', ['-c', command], {env:opts.env, cwd:opts.cwd});
-
-		// When the child process is done.
-		child.on('close', function (code) {
-			var err; // Only defined if an error occured
-
-			// Handle errors
-			if (code !== 0) {
-				var errorMessage = "`" + command + "` exited with code " + code;
-				err = new Error(errorMessage);
-				logger.log(1, errorMessage);
-			}
-
-			logger.close();
-
-			if (typeof callback === 'function') {
-				process.nextTick(callback.bind(undefined, err));
-			}
-		});
-
-		// Handle input.
-		if (input && typeof input.pipe === 'function') {
-			input.pipe(child.stdin);
-		} else if (input !== undefined && input !== null) {
-			child.stdin.end(input.toString());
-		} else {
-			child.stdin.end();
-		}
-
-		// Handle output.
-		outStream = new stream.Transform();
-		outStream._transform = function (chunk, enc, callback) {
-			outStream.push(chunk);
-			logger.write(2, chunk, enc, callback);
-		};
-		child.stdout.pipe(outStream);
-		child.stderr.pipe(logger.stream);
-
-		// Return a vinyl file wrapping the command's stdout.
-		return new Vinyl({
-			path: command.split(/\s+/)[0], // first word of the command
-			contents: outStream
-		});
-
+var GulpRunner = module.exports = function run(template, opts) {
+	if (!(this instanceof GulpRunner)) {
+		return new GulpRunner(template, opts);
 	}
 
+	var command = new Command(template, opts);
 
-	// The stream.Transform interface
-	// --------------------------------------------------
-	// This method is called automatically for each file piped into the stream. It spawns a
-	// command for each file, using the file's contents as stdin, and pushes downstream a new file
-	// wrapping stdout.
+	Transform.call(this, {
+		objectMode: true
+	});
 
-	commandStream._transform = function (file, enc, done) {
-		var output;
-
-		file.base = process.cwd();
-
-		// Spawn the command.
-		output = exec(commandTemplate({file:file}), file, function (err) {
-			if (err) commandStream.emit('error', err);
-			else process.nextTick(done);
-		});
-
-		// Push downstream a vinyl file wrapping the command's stdout.
-		commandStream.push(output);
+	this._transform = function _transform(file, enc, callback) {
+		var newfile = command.exec(file, callback);
+		this.push(newfile);
 	};
 
-
-	/// `cmd.exec([callback])`
-	/// --------------------------------------------------
-	/// Executes the command immediately, returning a stream of vinyl. A single file containing
-	/// the command's stdout is pushed down the stream.
-	///
-	/// The name of the file pushed down the stream is the first word of the command.
-	/// See [gulp-rename] if you need more flexibility.
-	///
-	/// ### Arguments
-	/// 1. `[callback]` *(Function)*: Execution is asynchronous. The callback is called once the
-	///     command's stdout has closed.
-	///
-	/// ### Returns
-	/// *(stream.Readable in Object Mode)*: A stream containing exactly one vinyl file. The file's
-	/// contents is the stdout stream of the command.
-	///
-	/// ### Example
-	/// ```javascript
-	/// gulp.task('hello-world', function () {
-	///     run('echo Hello World').exec() // prints "[echo] Hello World\n"
-	///         .pipe(gulp.dest('output')) // Writes "Hello World\n" to output/echo
-	/// })
-	/// ```
-
-	commandStream.exec = function (callback) {
-		var output; // A vinyl file whose contents is the stdout of the command.
-		var wrapper; // The higher-level vinyl stream. `output` is the only thing piped through.
-
-		// Spawn the command.
-		output = exec(commandTemplate(), null, function (err) {
-			if (err) wrapper.emit('error', err);
-			if (typeof callback === 'function') {
-				process.nextTick(callback.bind(undefined, err));
-			}
-		});
-
-		// Wrap the output in a vinyl stream.
-		wrapper = new stream.PassThrough({objectMode:true});
-		wrapper.end(output);
-
-		return wrapper;
+	this.exec = function exec(stdin, callback) {
+		this.write(stdin, callback);
+		return this;
 	};
 
-	return commandStream;
 };
+
+GulpRunner.prototype = Transform.prototype;
+
+GulpRunner.Command = Command;
