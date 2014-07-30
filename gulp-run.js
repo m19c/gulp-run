147328fbb6b0618f150f01d5f37b39f2a16f71f8
Chris Barrick <cbarrick1@gmail.com>
2014-07-29 23:54:21 -0400

    :lipstick:

--- gulp-run.js
+++ gulp-run.js
@@ -1,6 +1,6 @@
-/// gulp-run
-/// ==================================================
-/// Pipe to shell commands in gulp.
+// gulp-run
+// ==================================================
+// Pipe to shell commands in gulp.
 
 'use strict';
 
