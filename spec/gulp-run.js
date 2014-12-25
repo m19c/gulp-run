10e30f8db52c7ca816797665d7945d5758379bfc
Chris Barrick <cbarrick1@gmail.com>
2014-12-25 09:55:33 -0500

    Add test for #18

--- spec/gulp-run.js
+++ spec/gulp-run.js
@@ -6,6 +6,8 @@ var pathlib = require('path');
 var Stream = require('stream');
 var expect = require('chai').expect;
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+
 var run = require('../');
 
 
@@ -20,20 +22,16 @@ describe('gulp-run', function () {
 
 	it('includes `node_modules/.bin` on the PATH', function (done) {
 		var nodeModulesPath = pathlib.join(__dirname, '..', '..', '.bin');
-
 		run('echo $PATH', {verbosity:0}).exec()
 			.pipe(compare(new RegExp('^' + nodeModulesPath)))
 			.pipe(call(done));
-
 	});
 
 
 	it('lets you set the initial cwd of the command', function (done) {
-
 		run('pwd', {cwd:'/', verbosity:0}).exec()
 			.pipe(compare('/\n'))
 			.pipe(call(done));
-
 	});
 
 
@@ -75,50 +73,41 @@ describe('gulp-run', function () {
 			expect(output).to.match(/\[.*\] \$ echo "testing verbosity:2"\s*\ntesting verbosity:2/);
 			almostDone();
 		});
-
 	});
 
 
 	describe('in a vinyl pipeline', function () {
 
 		it('works with buffers', function (done) {
-
 			gulp.src(sampleFilename, {buffer:true})            // Each line is the line number.
 				.pipe(run('awk "NR % 2 == 0"', {verbosity:0})) // Get the even lines with awk.
 				.pipe(compare('2\n4\n6\n8\n10\n12\n'))         // Compare the output.
 				.pipe(call(done));                             // Profit.
-
 		});
 
 
 		it('works with streams', function (done) {
-
 			gulp.src(sampleFilename, {buffer:false})           // Each line is the line number.
 				.pipe(run('awk "NR % 2 == 0"', {verbosity:0})) // Get the even lines with awk.
 				.pipe(compare('2\n4\n6\n8\n10\n12\n'))         // Compare the output.
 				.pipe(call(done));                             // Profit.
-
 		});
 
 
 		it('supports command templates, i.e. `echo <%= file.path %>`', function (done) {
-
 			gulp.src(sampleFilename)
 				.pipe(run('echo <%= file.path %>', {verbosity:0})) // echo the name of the file.
 				.pipe(compare(sampleFilename + '\n'))
 				.pipe(call(done));
-
 		});
 
 
 		it('emits an `error` event on a failed command', function (done) {
-
 				gulp.src(sampleFilename)
 					.pipe(run('exit 1', {verbosity:0})) // Non-zero exit code
 					.on('error', function () {
 						done();
 					});
-
 		});
 
 	});
@@ -127,35 +116,38 @@ describe('gulp-run', function () {
 	describe('direct execution (`.exec`)', function () {
 
 		it('is asynchronous (this test sleeps for 1s)', function (done) {
-
 			var startTime = process.hrtime()[0]; // Current time in seconds
-
-			// Sleep for 1s, then callback
 			run('sleep 1', {verbosity:0}).exec(function () {
 				var delta = process.hrtime()[0] - startTime; // Time in seconds
 				expect(delta).to.equal(1);
 				done();
 			});
-
 		});
 
 
 		it('returns a vinyl stream wrapping stdout', function (done) {
-
 			run('echo Hello World', {verbosity:0}).exec() // Start a command with `.exec()`.
 				.pipe(compare('Hello World\n'))           // stdout piped as a Vinyl file.
 				.pipe(call(done));
-
 		});
 
 
 		it('emits an `error` event on a failed command', function (done) {
-
 			run('exit 1', {verbosity:0}).exec() // Non-zero exit code
 				.on('error', function () {
 					done();
 				});
+		});
 
+	});
+
+
+	describe('reported issues', function () {
+
+		it('#18', function (done) {
+			run("echo hello world", {cwd: './', verbosity:0}).exec()
+				.pipe(rename("dest.txt"))
+				.pipe(call(done));
 		});
 
 	});
