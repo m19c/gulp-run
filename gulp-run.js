98ad0d1051967d6c72f0dd59098e0dd6e48bf0eb
Chris Barrick <cbarrick1@gmail.com>
2015-03-25 15:27:43 -0400

    :lipstick: improve comments

--- gulp-run.js
+++ gulp-run.js
@@ -1,6 +1,6 @@
 // gulp-run
 // ==================================================
-// Pipe to shell commands in gulp.
+// Pipe shell commands in gulp.
 
 'use strict';
 
@@ -9,20 +9,24 @@ var Transform = require('stream').Transform;
 var Command = require('./lib/command');
 
 
-// run(template, [opts])  /  new GulpRunner(template, [opts])
+// run(template, [opts])
+// new GulpRunner(template, [opts])
 // --------------------------------------------------
-// Creates a vinyl stream that spawns a command to process the input.
+// Creates a GulpRunner.
+//
+// A GulpRunner is a Vinyl transform stream that spawns a child process to
+// transform the file. A separate process is spawned to handle each file
+// passing through the stream.
 
 var GulpRunner = module.exports = function run(template, opts) {
 	if (!(this instanceof GulpRunner)) {
 		return new GulpRunner(template, opts);
 	}
 
-	// The command to run when processing input.
 	var command = new Command(template, opts);
 
-	// A GulpRunner is a Vinyl transform stream that uses the `command` to process input.
 	Transform.call(this, {objectMode:true});
+
 	this._transform = function _transform(file, enc, callback) {
 		var newfile = command.exec(file, callback);
 		this.push(newfile);
@@ -31,9 +35,15 @@ var GulpRunner = module.exports = function run(template, opts) {
 
 	// GulpRunner#exec([stdin], [callback])
 	// --------------------------------------------------
-	// Writes stdin to itself, causing the command to be executed with that input.
+	// Writes `stdin` to itself and returns itself.
+	//
+	// Whenever an object is written into the GulpRunner, a new process is
+	// spawned taking that data as standard input, and a Vinyl file wrapping the
+	// process's standard output is pushed downstream.
+	//
+	// `stdin` may be a String, Buffer, Readable stream, or Vinyl file.
 
-	this.exec = function exec(stdin, callback) {
+	this.exec = function (stdin, callback) {
 		this.write(stdin, callback);
 		return this;
 	};
