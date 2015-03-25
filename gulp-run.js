531afc3a38220502728dc65c852c84a66ff84659
Chris Barrick <cbarrick1@gmail.com>
2015-03-25 15:33:47 -0400

    End the stream after run(...).exec()

--- gulp-run.js
+++ gulp-run.js
@@ -45,6 +45,7 @@ var GulpRunner = module.exports = function run(template, opts) {
 
 	this.exec = function (stdin, callback) {
 		this.write(stdin, callback);
+		this.push(null);
 		return this;
 	};
 
