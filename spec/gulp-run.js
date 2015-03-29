7d815597ec76c123536fd6603e28995beb392b3c
Chris Barrick <cbarrick1@gmail.com>
2015-03-29 12:25:38 -0400

    Update tests

--- spec/gulp-run.js
+++ spec/gulp-run.js
@@ -11,10 +11,6 @@ var rename = require('gulp-rename');
 var run = require('../');
 
 
-// We have a lot of loggers listening on stdout
-// process.stdout.setMaxListeners(Infinity);
-
-
 describe('gulp-run', function () {
 
 	var sampleFilename = pathlib.join(__dirname, 'sample.input.txt');
@@ -24,21 +20,21 @@ describe('gulp-run', function () {
 		var nodeModulesPath = pathlib.join(__dirname, '..', '..', '.bin');
 		run('echo $PATH', {verbosity:0}).exec()
 			.pipe(compare(new RegExp('^' + nodeModulesPath)))
-			.pipe(call(done));
+			.on('finish', done);
 	});
 
 
 	it('lets you set the initial cwd of the command', function (done) {
 		run('pwd', {cwd:'/', verbosity:0}).exec()
 			.pipe(compare('/\n'))
-			.pipe(call(done));
+			.on('finish', done);
 	});
 
 
 	it('supports verbosity levels', function (done) {
 		var colors = require('gulp-util').colors;
 
-		// Stub out stdout.write
+		// Stub out stdout.write to write into the string `writtenOutput`
 		var stdoutWrite = process.stdout.write;
 		var writtenOutput = '';
 		process.stdout.write = function (chunk, enc, callback) {
@@ -46,68 +42,68 @@ describe('gulp-run', function () {
 			if (typeof callback === 'function') process.nextTick(callback);
 		};
 
-		var count = 0;
-		function almostDone() {
-			count += 1;
-			if (count >= 3) {
-				process.stdout.write = stdoutWrite;
-				return done();
-			}
-		}
-
-		(new run.Command('echo "testing verbosity:0"', {verbosity:0})).exec(function () {
-			expect(writtenOutput).to.match(/^$/);
-			writtenOutput = '';
-			almostDone();
+		var s = new Semaphore(3, function() {
+			// Restore process.stdout before exiting
+			process.stdout.write = stdoutWrite;
+			return done();
 		});
 
-		(new run.Command('echo "testing verbosity:1"', {verbosity:1})).exec(function () {
-			var output = colors.stripColor(writtenOutput);
-			expect(output).to.match(/\[.*\] \$ echo "testing verbosity:1" # Silenced\s*\n/);
-			writtenOutput = '';
-			almostDone();
-		});
+		(new run.Command('echo "testing verbosity:0"', {verbosity:0}))
+			.exec(function () {
+				expect(writtenOutput).to.match(/^$/);
+				writtenOutput = '';
+				s.done();
+			});
 
-		(new run.Command('echo "testing verbosity:2"', {verbosity:2})).exec(function () {
-			var output = colors.stripColor(writtenOutput);
-			expect(output).to.match(/\[.*\] \$ echo "testing verbosity:2"\s*\ntesting verbosity:2/);
-			almostDone();
-		});
+		(new run.Command('echo "testing verbosity:1"', {verbosity:1}))
+			.exec(function () {
+				var output = colors.stripColor(writtenOutput);
+				expect(output).to.match(/\[.*\] \$ echo "testing verbosity:1" # Silenced\s*\n/);
+				writtenOutput = '';
+				s.done();
+			});
+
+		(new run.Command('echo "testing verbosity:2"', {verbosity:2}))
+			.exec(function () {
+				var output = colors.stripColor(writtenOutput);
+				expect(output).to.match(/\[.*\] \$ echo "testing verbosity:2"\s*\ntesting verbosity:2/);
+				s.done();
+			});
 	});
 
 
 	describe('in a vinyl pipeline', function () {
 
 		it('works with buffers', function (done) {
-			gulp.src(sampleFilename, {buffer:true})            // Each line is the line number.
-				.pipe(run('awk "NR % 2 == 0"', {verbosity:0})) // Get the even lines with awk.
-				.pipe(compare('2\n4\n6\n8\n10\n12\n'))         // Compare the output.
-				.pipe(call(done));                             // Profit.
+			// Use awk to extract the even lines of a file
+			gulp.src(sampleFilename, {buffer:true})
+				.pipe(run('awk "NR % 2 == 0"', {verbosity:0}))
+				.pipe(compare('2\n4\n6\n8\n10\n12\n'))
+				.on('finish', done);
 		});
 
 
 		it('works with streams', function (done) {
-			gulp.src(sampleFilename, {buffer:false})           // Each line is the line number.
-				.pipe(run('awk "NR % 2 == 0"', {verbosity:0})) // Get the even lines with awk.
-				.pipe(compare('2\n4\n6\n8\n10\n12\n'))         // Compare the output.
-				.pipe(call(done));                             // Profit.
+			// Use awk to extract the even lines of a file
+			gulp.src(sampleFilename, {buffer:false})
+				.pipe(run('awk "NR % 2 == 0"', {verbosity:0}))
+				.pipe(compare('2\n4\n6\n8\n10\n12\n'))
+				.on('finish', done);
 		});
 
 
 		it('supports command templates, i.e. `echo <%= file.path %>`', function (done) {
 			gulp.src(sampleFilename)
-				.pipe(run('echo <%= file.path %>', {verbosity:0})) // echo the name of the file.
+				.pipe(run('echo <%= file.path %>', {verbosity:0}))
 				.pipe(compare(sampleFilename + '\n'))
-				.pipe(call(done));
+				.on('finish', done);
 		});
 
 
 		it('emits an `error` event on a failed command', function (done) {
 				gulp.src(sampleFilename)
-					.pipe(run('exit 1', {verbosity:0})) // Non-zero exit code
-					.on('error', function () {
-						done();
-					});
+					.pipe(run('exit 1', {verbosity:0}))
+					.on('error', noop(done));
 		});
 
 	});
@@ -119,32 +115,36 @@ describe('gulp-run', function () {
 			var s = new Semaphore(2, done);
 			var startTime = process.hrtime()[0]; // Current time in seconds
 
-			// We spawn two sleeps that run in parallel
-			run('sleep 1', {verbosity:0}).exec(function () {
-				var delta = process.hrtime()[0] - startTime;
-				expect(delta).to.equal(1);
-				s.done();
-			});
-			run('sleep 1', {verbosity:0}).exec(function () {
-				var delta = process.hrtime()[0] - startTime;
-				expect(delta).to.equal(1);
-				s.done();
-			});
+			for (var i = 0; i < 2; i += 1) {
+				run('sleep 1', {verbosity:0})
+					.exec(function () {
+						var delta = process.hrtime()[0] - startTime;
+						expect(delta).to.equal(1);
+						s.done();
+					});
+			}
 		});
 
 
 		it('returns a vinyl stream wrapping stdout', function (done) {
-			run('echo Hello World', {verbosity:0}).exec() // Start a command with `.exec()`.
-				.pipe(compare('Hello World\n'))           // stdout piped as a Vinyl file.
-				.pipe(call(done));
+			run('echo Hello World', {verbosity:0})
+				.exec()
+				.pipe(compare('Hello World\n'))
+				.on('finish', done);
 		});
 
 
 		it('emits an `error` event on a failed command', function (done) {
-			run('exit 1', {verbosity:0}).exec() // Non-zero exit code
-				.on('error', function () {
-					done();
-				});
+			run('exit 1', {verbosity:0})
+				.exec()
+				.on('error', noop(done));
+		});
+
+
+		it('closes the stream when done', function (done) {
+			run('echo Hello World', {verbosity:0})
+				.exec()
+				.on('finish', done);
 		});
 
 	});
@@ -152,10 +152,10 @@ describe('gulp-run', function () {
 
 	describe('reported issues', function () {
 
-		it('#18', function (done) {
+		it('#18 -- file names and paths', function (done) {
 			run("echo hello world", {cwd: './', verbosity:0}).exec()
 				.pipe(rename("dest.txt"))
-				.pipe(call(done));
+				.on('finish', done);
 		});
 
 	});
@@ -166,29 +166,31 @@ describe('gulp-run', function () {
 /// Helpers
 /// --------------------------------------------------
 
+// Returns a no-op function. If a callback is given, it is called when the
+// returned function is called. This is useful for wrapping functions to ignore
+// arguments.
+function noop(callback) {
+	if (typeof callback != 'function') {
+		callback = function(){};
+	}
+	return function () {
+		callback()
+	};
+}
+
 // Constructs an async-semaphore that calls back when semaphore#done()
-// has been called a given number of times
+// has been called a given number of times.
 function Semaphore(count, callback) {
 	this.done = function () {
-		return (--count <= 0) ? callback() : this;
-	}
+		count -= 1;
+		return (count <= 0) ? callback() : this;
+	};
 	return this;
 }
 
 
-// A stream that calls a function whenever a file is piped in.
-function call(callback1) {
-	var stream = new Stream.Transform({objectMode:true});
-	stream._transform = function (file, enc, callback2) {
-		this.push(file);
-		callback1();
-		process.nextTick(callback2);
-	};
-	return stream;
-}
-
-
-// A stream that throws if the contents of the incoming file doesn't match the argument.
+// Returns a pass-through Vinyl stream that throws an error if the contents of
+// the incoming file doesn't match a pattern.
 function compare(match) {
 	if (!(match instanceof RegExp)) {
 		match = new RegExp('^' + match.toString() + '$');
